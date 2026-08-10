ccd2944a3b9963cd675e95f246b4b0170c84bd6c
Adham <adham@paymob.com>
2026-08-10 09:51:32 +0300

    feat(tools): add CloudBeaver container service and enable DRF Browsable API UI in dev

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -2,5 +2,11 @@ from .base import *
 
 DEBUG = True
 
+# Enable DRF Browsable API UI in Development
+REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = (
+    'rest_framework.renderers.JSONRenderer',
+    'rest_framework.renderers.BrowsableAPIRenderer',
+)
+
 # Development email backend (prints to console)
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
